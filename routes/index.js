6a6094cb225ab3b6d9839288712bc9fc97553b46
thisIsJooS <thisis.joos@gmail.com>
2022-06-10 23:26:31 +0900

    경매 종료 후 낙찰자 선정

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const schedule = require("node-schedule");
 
-const { Good, Auction, User } = require("../models");
+const { Good, Auction, User, sequelize } = require("../models");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 
 const router = express.Router();
@@ -67,12 +68,34 @@ router.post(
   async (req, res, next) => {
     try {
       const { name, price } = req.body;
-      await Good.create({
+      const good = await Good.create({
         OwnerId: req.user.id,
         name,
         img: req.file.filename,
         price,
       });
+      const end = new Date();
+      end.setDate(end.getDate() + 1);
+      schedule.scheduleJob(end, async () => {
+        const success = await Auction.findOne({
+          where: { GoodId: good.id },
+          order: [["bid", "DESC"]],
+        });
+        await Good.update(
+          { SoldId: success.UserId },
+          { where: { id: good.id } }
+        );
+        await User.update(
+          {
+            // 낙찰자의 보유 자산을 낙찰 금액만큼 뺀다
+            money: sequelize.literal(`money - ${success.bid}`),
+          },
+          {
+            where: { id: success.UserId },
+          }
+        );
+      });
+
       res.redirect("/");
     } catch (error) {
       console.error(error);
